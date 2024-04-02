46644c276c666a2db8ed963218975f5329f571fb
Joel Lamata <jlamata88@gmail.com>
2024-04-02 14:27:07 +0200

    isactive

--- docs/marcadores.js
+++ docs/marcadores.js
@@ -1,11 +1,18 @@
 var markersData = [
     {
       "city": "Waterdeep",
-      "coordenadas": [74.179817, 149.076233]
+      "coordenadas": [74.179817, 149.076233],
+      "isActive": true
     },
     {
       "city": "Candlekeep",
-      "coordenadas": [44.731126, 167.937012]
+      "coordenadas": [44.731126, 167.937012],
+      "isActive": true
+    },
+    {
+      "city": "Baldur's Gate",
+      "coordenadas": [53.481508, 173.125305],
+      "isActive": true
     }
     // Agrega más marcadores según sea necesario
 ]
