a68d04ae75414044f8964c9242a46b9da2cec619
Joel Lamata <jlamata88@gmail.com>
2024-04-02 16:56:25 +0200

    opcion cambio de colores

--- docs/marcadores.js
+++ docs/marcadores.js
@@ -1,17 +1,26 @@
 var markersData = [
     {
-      "city": "Waterdeep",
+      "name": "Waterdeep",
       "coordenadas": [74.179817, 149.076233],
+      "color": "blue",
       "isActive": true
     },
     {
-      "city": "Candlekeep",
+      "name": "Candlekeep",
       "coordenadas": [44.731126, 167.937012],
+      "color": "blue",
       "isActive": true
     },
     {
-      "city": "Baldur's Gate",
+      "name": "Baldur's Gate",
       "coordenadas": [53.481508, 173.125305],
+      "color": "blue",
+      "isActive": true
+    },
+    {
+      "name": "The Cloakwood",
+      "coordenadas": [49.296472, 169.189453],
+      "color": "green",
       "isActive": true
     }
     // Agrega más marcadores según sea necesario
