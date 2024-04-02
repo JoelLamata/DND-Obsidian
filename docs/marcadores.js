d77f496f6d6c14f30122f33dc8300336d08cc974
Joel Lamata <jlamata88@gmail.com>
2024-04-02 13:58:42 +0200

    vault backup: 2024-04-02 13:58:42

---
+++ docs/marcadores.js
@@ -0,0 +1,13 @@
+var markersData = [
+    {
+      "city": "Waterdeep",
+      "coordenadas": [74.179817, 149.076233]
+    },
+    {
+      "city": "Candlekeep",
+      "coordenadas": [44.731126, 167.937012]
+    }
+    // Agrega más marcadores según sea necesario
+]
+  
+  
