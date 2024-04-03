d68d3077dc98105b4c636a36646173e64d62aed2
Joel Lamata <jlamata88@gmail.com>
2024-04-03 16:44:01 +0200

    filtro de marcadores

--- docs/marcadores.js
+++ docs/marcadores.js
@@ -1,29 +1,54 @@
+const MarkerTypes = {
+  CITY: 'city',
+  RIVER: 'river',
+  FOREST: 'forest',
+  MOUNTAIN: 'mountain'
+};
+
 var markersData = [
     {
       "name": "Waterdeep",
-      "coordenadas": [74.179817, 149.076233],
-      "color": "blue",
+      "coordinates": [74.179817, 149.076233],
+      "type": MarkerTypes.CITY,
       "isActive": true
     },
     {
       "name": "Candlekeep",
-      "coordenadas": [44.731126, 167.937012],
-      "color": "blue",
+      "coordinates": [44.731126, 167.937012],
+      "type": MarkerTypes.CITY,
       "isActive": true
     },
     {
       "name": "Baldur's Gate",
-      "coordenadas": [53.481508, 173.125305],
-      "color": "blue",
+      "coordinates": [53.481508, 173.125305],
+      "type": MarkerTypes.CITY,
       "isActive": true
     },
     {
       "name": "The Cloakwood",
-      "coordenadas": [49.296472, 169.189453],
-      "color": "green",
+      "coordinates": [49.296472, 169.189453],
+      "type": MarkerTypes.FOREST,
       "isActive": true
+    },
+    {
+      "name": "Wood of Sharp Teeth",
+      "coordinates": [47.428087, 188.393555],
+      "type": MarkerTypes.FOREST,
+      "isActive": true
+    },
+    {
+      "name": "The Cloud Peaks",
+      "coordinates": [33.85217, 178.374023],
+      "type": MarkerTypes.MOUNTAIN,
+      "isActive": true
+    },
+    {
+      "name": "TODO",
+      "coordinates": [47.428087, 188.393555],
+      "type": MarkerTypes.FOREST,
+      "isActive": false
     }
-    // Agrega más marcadores según sea necesario
+
 ]
   
   
